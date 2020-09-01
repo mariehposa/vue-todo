9d7ea3387496d15d49d38ceb212841ee9b15e0f4
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2020-09-01 14:51:24 +0100

    wrap app inside theme provider

--- todo/src/main.js
+++ todo/src/main.js
@@ -3,3 +3,8 @@ import Chakra, { CThemeProvider } from '@chakra-ui/vue'
 import App from './App.vue'
 
 createApp(App).mount('#app')
+
+new Vue({
+    el: '#app',
+    render: (h) => h(CThemeProvider, [h(App)])
+  }).$mount()
